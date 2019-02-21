d2f11b21be7ffa91acd15ed73eac63b3b80e2eee
garikwamp <garikwamp31@gmail.com>
2019-02-21 14:22:19 +0200

    git add

---
+++ hometask1/src/com/epam/hometask1/Main.java
@@ -0,0 +1,12 @@
+package com.epam.hometask1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("* * * *    * * * *      *       *       *");
+        System.out.println("*          *     *    *   *     * *   * *");
+        System.out.println("* *        * * * *   *     *    *   *   *");
+        System.out.println("*          *        * * * * *   *       *");
+        System.out.println("* * * *    *       *         *  *       *");
+    }
+}
